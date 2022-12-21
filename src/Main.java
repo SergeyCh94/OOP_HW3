f02a28d50ca01a30e0f399f960fbf5b2387f64d5
helloworld <sergeychernonosov21229@gmail.com>
2022-12-21 18:46:41 +0300

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import transport.Car;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Car ladaCar = new Car("Lada", "Granta", 1.7, "Желтый", 2015, "Россия", "Автомат", "Седан",
+                "1234-IH3", 5, true);
+        Car audiCar = new Car("Audi", "A8 50 L TDI quattro", 3.0, "Черный", 2020, "Германия", "Автомат",
+                "Универсал", "1212 - IH3", 5, false);
+        Car bmwCar = new Car("BMW", "Z8", 3.0, "Черный", 2021, "Германия", "Автомат",
+                "Универсал", "1212 - IH3", 5, true);
+        Car kiaCar = new Car("KIA", "Sportage 4-го поколения", 2.4, "Красный", 2018, "Южная Корея", "Автомат",
+                "Универсал", "1212 - IH3", 5, true);
+        Car hyundaiCar = new Car("Hyundai", "Avante", 1.6, "Оранжевый", 2015, "Южная Корея", "Автомат",
+                "Универсал", "1212 - IH3", 5, false);
+
+        System.out.println(ladaCar);
+        System.out.println(audiCar);
+        System.out.println(bmwCar);
+        System.out.println(kiaCar);
+        System.out.println(hyundaiCar);
+        ladaCar.changeTiers(9);
+        System.out.println(ladaCar);
+
+    }
+}
---
+++ src/transport/Car.java
@@ -0,0 +1,232 @@
+package transport;
+
+
+import java.time.LocalDate;
+
+import java.util.Objects;
+
+public class Car {
+    private final String brand;
+    private final String model;
+    private double engineVolume;
+    private String color;
+    private final int year;
+    private final String country;
+    private String transmission;
+    private final String bodyType;
+    private String registrationNumber;
+    private final int numberOfSeats;
+    private boolean summerTiers;
+
+
+    @Override
+    public String toString() {
+        return getBrand() + " " + getModel() + ", " + getYear() + " год выпуска, сборка в " + getCountry() + ", " +
+                getColor() + " цвет кузова, обьем двигателя " + getEngineVolume() + " - л." + "\n" + "Коробка передач " + getTransmission() +
+                ", Тип кузова " + getBodyType() + ", Регистрационный номер " + getRegistrationNumber() + ", Количество мест " +
+                getNumberOfSeats() + ", Тип резины летняя " + summerTiers;
+    }
+
+    public void changeTiers(int month) {
+        if (month <= 0 || month > 12){
+            System.out.println("Такого месяца нет");
+        } else {
+            summerTiers = month >= 5 && month < 9;
+        }
+    }
+
+    public Car(String brand, String model, double engineVolume, String color, int year, String country, String transmission,
+               String bodyType, String registrationNumber, int numberOfSeats, boolean summerTiers) {
+
+        if (numberOfSeats <= 0){
+            this.numberOfSeats = 1;
+        } else {
+            this.numberOfSeats = numberOfSeats;
+        }
+
+        if (registrationNumber == null || registrationNumber.isEmpty()){
+            this.registrationNumber = "default";
+        } else {
+            this.registrationNumber = registrationNumber;
+        }
+
+        if (bodyType == null || bodyType.isEmpty()){
+            this.bodyType = "default";
+        } else {
+            this.bodyType = bodyType;
+        }
+
+        if (transmission == null || transmission.isEmpty()){
+            this.transmission = "default";
+        } else {
+            this.transmission = transmission;
+        }
+
+        if (brand == null || brand.isEmpty()){
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+
+        if (model == null || model == ""){
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+
+        if (country == null || country == ""){
+            this.country = "default";
+        } else {
+            this.country = country;
+        }
+
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+
+        if (color == null || color == "") {
+            this.color = "Белый";
+        } else {
+            this.color = color;
+        }
+
+        if (year < 0) {
+            this.year = 2000;
+        } else {
+            this.year = year;
+        }
+
+        this.summerTiers = summerTiers;
+
+    }
+
+    public class Key {
+        private final boolean remoteEngineStart;
+        private final boolean keylessAccess;
+
+        public Key(boolean remoteEngineStart, boolean keylessAccess) {
+
+            this.remoteEngineStart = remoteEngineStart;
+            this.keylessAccess = keylessAccess;
+
+        }
+        public boolean isRemoteEngineStart() {
+            return remoteEngineStart;
+        }
+
+        public boolean isKeylessAccess() {
+            return keylessAccess;
+        }
+    }
+
+    public class Insurance {
+        private final LocalDate validityPeriod;
+        private final double cost;
+        private final String number;
+
+        public Insurance(LocalDate validityPeriod, double cost, String number) {
+            this.validityPeriod = Objects.requireNonNullElseGet(validityPeriod, LocalDate::now);
+            if (cost >= 0) {
+                this.cost = cost;
+            } else {
+                this.cost = 0;
+            }
+            if (number == null || number.isEmpty()) {
+                this.number = "default";
+            } else {
+                this.number = number;
+            }
+        }
+
+        public void checkExpirationDate() {
+            if (validityPeriod.isBefore(LocalDate.now().minusDays(365))) {
+                System.out.println("Нужно менять страховку");
+            }
+        }
+
+        public void checkNumber() {
+            if (number.length() != 9) {
+                System.out.println("Неверный номер");
+            }
+        }
+
+        public LocalDate getValidityPeriod() {
+            return validityPeriod;
+        }
+
+        public double getCost() {
+            return cost;
+        }
+
+        public String getNumber() {
+            return number;
+        }
+    }
+
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public boolean isSummerTiers() {
+        return summerTiers;
+    }
+
+    public void setSummerTiers(boolean summerTiers) {
+        this.summerTiers = summerTiers;
+    }
+}
